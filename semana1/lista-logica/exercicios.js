27076f637149efc316b3e09788fd6560dd2a017c
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2021-06-18 16:30:02 -0300

    Lista-de-Introdução-ao-Js

---
+++ semana1/lista-logica/exercicios.js
@@ -0,0 +1,141 @@
+// // // // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// // // // EXERCÍCIO 0A
+// // // function soma(num1, num2) {
+// // //   // implemente sua lógica aqui
+// // //   return num1 + num2
+// // // }
+
+// // // // EXERCÍCIO 0B
+// // // function imprimeMensagem() {
+// // //   // implemente sua lógica aqui
+// // //   const mensagem = prompt('Digite uma mensagem!')
+
+// // //   console.log(mensagem)
+// // // }
+
+// // // // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// // // // EXERCÍCIO 01
+// // // function calculaAreaRetangulo () {
+// // //   let altura = Number(prompt("Qual a altura?"))
+// // //   let largura = Number(prompt("Qual a altura?"))
+// // //   console.log(altura * largura)
+
+// // // }
+
+// // // // EXERCÍCIO 02
+
+// // // function imprimeIdade() {
+// // //   const anoNascimento = Number(prompt("Qual o ano que você nasceu?"))
+// // //   const anoAtual = Number (prompt("Qual o ano atual?"))
+// // //   const subtracao = Number (anoNascimento - anoAtual)
+// // //   console.log(subtracao)
+// // //   return subtracao
+// // // }
+
+// // //  // EXERCÍCIO 03
+// // // function calculaIMC(peso, altura) {
+// // //   const imc = peso /(altura * altura)
+// // //   return imc
+// // // }
+
+// // // // EXERCÍCIO 04
+// // // function imprimeInformacoesUsuario() {
+// // //   const nomeUsuario = (prompt("Qual seu nome?"))
+// // //   const idadeUsuario = Number (prompt("Qual sua idade?"))
+// // //   const emailUsuario = (prompt("Qual seu email?"))
+// // //   console.log (`Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e o meu email é ${emailUsuario}.`)
+
+// // //   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+// // // }
+
+// // // // EXERCÍCIO 05
+// // // function imprimeTresCoresFavoritas() {
+// // //   const cor1 = prompt["Qual a sua cor favorita?"]
+// // //   const cor2 = prompt["Qual a sua cor favorita?"]
+// // //   const cor3 = prompt["Qual a sua cor favorita?"]
+// // //   console.log(["Laranja","Roxo","Preto"])
+// // // }
+
+// // // // EXERCÍCIO 06
+// //   function retornaStringEmMaiuscula(string) {
+// //   const normalCaixa = string
+// //   const caixaAlterada = normalCaixa.toUpperCase() 
+// //   return caixaAlterada
+
+// // }
+
+// // // EXERCÍCIO 07
+// // function calculaIngressosEspetaculo(custo, valorIngresso) {
+// //    const semPreju = custo / valorIngresso
+// //    calculaIngressosEspetaculo = semPreju
+// //    return calculaIngressosEspetaculo
+
+   
+// // }
+
+// // // EXERCÍCIO 08
+// // function checaStringsMesmoTamanho(string1, string2) {
+// //   const tamanhoFuncao = string1 >= string2 
+// //   checaStringsMesmoTamanho = tamanhoFuncao
+// //   return checaStringsMesmoTamanho
+
+// // }
+
+// // EXERCÍCIO 09
+// function retornaPrimeiroElemento(array) {
+//   const retornarArray = array
+//   retornaPrimeiroElemento = retornarArray[0]
+//   return retornaPrimeiroElemento
+
+// }
+
+// // EXERCÍCIO 10
+// function retornaUltimoElemento(array) {
+//   const retornarUltimo = array
+//   retornaUltimoElemento = retornarUltimo.splice (3)
+//   return retornaUltimoElemento
+// }
+
+// // // EXERCÍCIO 11
+// function trocaPrimeiroEUltimo(array) {
+//   const troca = array[0]
+//     array[0] = array[array.length - 1]
+//     array[array.length - 1] = troca
+//     return array
+//   }
+
+
+
+// // EXERCÍCIO 12
+// function checaIgualdadeDesconsiderandoCase(string1, string2) {
+//   const checagem = (string1.toLowerCase() === string2.toLowerCase())
+//   checaIgualdadeDesconsiderandoCase = checagem 
+//   return checaIgualdadeDesconsiderandoCase
+
+
+// }
+
+// // EXERCÍCIO 13
+// function checaRenovacaoRG() {
+//   const anoAtual = (prompt("Qual o ano atual?"))
+//   const anoNascimento = Number (prompt("Qual seu ano de nascimento?"))
+//   const anoRg = (prompt("Qual ano de emissão do seu RG"))
+//   //////
+
+
+// }
+
+// // EXERCÍCIO 14
+// function checaAnoBissexto(ano) {
+//   // implemente sua lógica aqui
+
+// }
+
+// // EXERCÍCIO 15
+// function checaValidadeInscricaoLabenu() {
+//   // implemente sua lógica aqui
+
+// }/////////////////
+// ////Desculpem vou mandar outro PR quaando chegar, estou atrasado pro trabalho e não consegui terminar tudo!!!!
