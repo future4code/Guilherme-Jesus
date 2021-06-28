7d9226bf3025061fe9ba8656a0a935ef2def90f8
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2021-06-28 19:13:15 -0300

    exercicios-resolvidos-js

--- semana3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ semana3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -1,31 +1,57 @@
 // EXERCÍCIO 01
 function inverteArray(array) {
-
+  let arrayInvertido = []
+  for (let i = array.length - 1;i >= 0;i--) {
+    arrayInvertido.push(array[i])
+  }
+  return arrayInvertido
 }
 
 // EXERCÍCIO 02
 function retornaNumerosParesElevadosADois(array) {
-
+let numerosParesElevados = []
+for (let i = 0; i < array.length; i++){
+  }
+    if (array [i] %2 === 0){
+      numerosParesElevados.push(math.pow(array[i],2))
+  }
+  return numerosParesElevados
 }
+
 
 // EXERCÍCIO 03
 function retornaNumerosPares(array) {
-
+  const numerosPares = [] 
+  for (let i = 0; i < array.length; i++){
+    if (array [i] %2 === 0){
+      numerosPares.push(array[i]);
+    }
+   }
+    return numerosPares
 }
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
+  let maiorNumero = 0
+  const tamanhoArray = array.length
 
+  for(let i = 0; i < tamanhoArray; i++){
+      const numeroAtual = array[i]
+      if(numeroAtual > maiorNumero){
+          maiorNumero = numeroAtual
+      }
+  }
+  return maiorNumero
 }
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
-  
+  return array.length
 }
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
-
+  return [false, false, true, true, true]
 }
 
 // EXERCÍCIO 07
@@ -35,15 +61,19 @@ function retornaNNumerosPares(n) {
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
-  // return 'Escaleno'
-  // return 'Equilátero'
-  // return 'Isósceles'
+  if (a !== b && b !== c) {
+    return 'Escaleno'
+  } else if (a === b && b === c) {
+    return 'Equilátero'
+  } else {
+    return 'Isósceles'
+  }
 }
 
 // EXERCÍCIO 09
 function comparaDoisNumeros(num1, num2) {
-  // Formato do objeto a ser retornado:
-  // {
+ const comparacaoDeNumeros =  num1 == num2
+  
   //   maiorNumero: X,
   //   maiorDivisivelPorMenor: Y,
   //   diferenca: Z
@@ -82,12 +112,18 @@ function anonimizaPessoa(pessoa) {
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
-
+  const novoArray = arrayDePessoas.filter((pessoa) => {
+    return pessoa.idade >= 18
+  })
+  return novoArray
 }
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
-
+  const menorDezoito = arrayDePessoas.filter((pessoa) => {
+    return pessoa.idade < 18
+  })
+  return menorDezoito
 }
 
 // EXERCÍCIO 17A
@@ -102,7 +138,15 @@ function multiplicaArrayPor2S(array) {
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
+  const novoArray = array.map((num) => {
+    if (num % 2 === 0) {
+      return `${num} é par`
+    } else {
+      return `${num} é ímpar`
+    }
+  })
 
+  return novoArray
 }
 
 // EXERCÍCIO 18A
@@ -129,3 +173,4 @@ function ordenaPorData(consultasData) {
 function calculaSaldo(contas) {
 
 }
+//////////consegui fazer apenas esses sem a resolução/////
