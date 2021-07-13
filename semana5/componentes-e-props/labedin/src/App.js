62724c0cb98e1b8afa3724bfc74e54f90f171df4
Guilherme-Jesus <guicoutoo@outlook.com>
2021-07-13 13:34:53 -0300

    Coloquei meu nome, foto e minha descricao

--- semana5/componentes-e-props/labedin/src/App.js
+++ semana5/componentes-e-props/labedin/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import minhaFoto from './minhaFoto.png';
+
 
 function App() {
   return (
@@ -9,9 +11,9 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          imagem={minhaFoto}
+          nome="Guilherme Henrique Couto de Jesus" 
+          descricao="Oi, eu sou Guilherme. Sou estudante de Web Full Stack na Labenu, atualmente faço freelance de Motoboy, adoro o ramo de tecnologias e tenho muita curiosidade em aprender novas linguagens."
         />
         
         <ImagemButton 
