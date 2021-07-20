dcbb37d79841c7d773856afd386adf5856f44a8c
Guilherme-Jesus <guicoutoo@outlook.com>
2021-07-20 08:45:01 -0300

    ainda nao finalizei o arquivo

---
+++ semana6/labenu-forms/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Etapa1 from './Componentes/Etapa1';
+import Etapa2 from './Componentes/Etapa2';
+import Etapa3 from './Componentes/Etapa3';
+
+
+function App() {
+  return (
+    <Etapa1></Etapa1>
+  
+  );
+}
+
+export default App;
---
+++ semana6/labenu-forms/src/Componentes/Final.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+function Final() {
+    return (
+        <div>
+            <h1>O FORMULÁRIO ACABOU</h1>
+            <div>
+                <h4> Muito Obrigado por participar! Entraremos em contato!</h4>
+            </div>
+           
+        </div>
+    );
+
+
+
+
+}
+
+
+
+
+
+export default Final
