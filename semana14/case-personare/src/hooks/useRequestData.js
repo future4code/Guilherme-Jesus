ae4c2a88c8802ca7b53b93cc9b786f8e2107c427
Guilherme-Jesus <guicoutoo@outlook.com>
2021-09-13 18:02:10 -0300

    instalado axios

---
+++ semana14/case-personare/src/hooks/useRequestData.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+
+
+
+
+const useRequestData = (initialData)=>{
+const [data,setData]=useState(initialData)
+
+
+   useEffect(()=>{
+    axios
+
+
+
+   })
+
+
+}
--- semana14/case-personare/src/pages/TaroPage.js
+++ semana14/case-personare/src/pages/TaroPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 
-
-
 export const TarotPage = () =>{
+
+
+ 
+    const Cartas = (()=>{
+
+    })
+
+
+
 
 
     return (
